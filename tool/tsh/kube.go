a9ab77482e8e4ee95530cbba4c0b0392813c749b
xacrimon <joel.wejdenstal@gmail.com>
2021-11-06 00:33:10 +0100

    add skeleton kube sessions command

--- tool/tsh/kube.go
+++ tool/tsh/kube.go
@@ -46,6 +46,7 @@ type kubeCommands struct {
 	credentials *kubeCredentialsCommand
 	ls          *kubeLSCommand
 	login       *kubeLoginCommand
+	sessions    *kubeSessionsCommand
 }
 
 func newKubeCommand(app *kingpin.Application) kubeCommands {
@@ -54,8 +55,25 @@ func newKubeCommand(app *kingpin.Application) kubeCommands {
 		credentials: newKubeCredentialsCommand(kube),
 		ls:          newKubeLSCommand(kube),
 		login:       newKubeLoginCommand(kube),
+		sessions:    newKubeSessionsCommand(kube),
 	}
 	return cmds
+}
+
+type kubeSessionsCommand struct {
+	*kingpin.CmdClause
+}
+
+func newKubeSessionsCommand(parent *kingpin.CmdClause) *kubeSessionsCommand {
+	c := &kubeSessionsCommand{
+		CmdClause: parent.Command("sessions", "Get a list of active kubernetes sessions."),
+	}
+
+	return c
+}
+
+func (c *kubeSessionsCommand) run(cf *CLIConf) error {
+	panic("unimplemented")
 }
 
 type kubeCredentialsCommand struct {
