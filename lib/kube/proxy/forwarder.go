9c25da7541fe9a839208add234e8d3f8c5cf8641
xacrimon <joel.wejdenstal@gmail.com>
2021-12-01 14:44:14 +0100

    fixed ghost session bug for exec api

--- lib/kube/proxy/forwarder.go
+++ lib/kube/proxy/forwarder.go
@@ -1098,6 +1098,15 @@ func (f *Forwarder) exec(ctx *authContext, w http.ResponseWriter, req *http.Requ
 =======
 =======
 
+	sess, err := f.newClusterSession(*ctx)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	if err := f.setupForwardingHeaders(sess, req); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
 	proxy, err := createRemoteCommandProxy(request)
 <<<<<<< HEAD
 >>>>>>> 5ea918098 (hook up streams from kube exec endpoint)
@@ -1107,9 +1116,14 @@ func (f *Forwarder) exec(ctx *authContext, w http.ResponseWriter, req *http.Requ
 		return nil, trace.Wrap(err)
 	}
 
+	if sess.noAuditEvents {
+		// We're forwarding this to another kubernetes_service instance, let it handle multiplexing.
+		return f.remoteExec(ctx, w, req, p, sess, request, proxy)
+	}
+
 	client := newKubeProxyClientStreams(proxy)
 	party := newParty(*ctx, client)
-	session, err := newSession(*ctx, f, req, p, party)
+	session, err := newSession(*ctx, f, req, p, party, sess)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
@@ -1139,6 +1153,24 @@ func (f *Forwarder) exec(ctx *authContext, w http.ResponseWriter, req *http.Requ
 =======
 	delete(f.sessions, session.id)
 >>>>>>> 10660dc65 (various fixes)
+	return nil, nil
+}
+
+func (f *Forwarder) remoteExec(ctx *authContext, w http.ResponseWriter, req *http.Request, p httprouter.Params, sess *clusterSession, request remoteCommandRequest, proxy *remoteCommandProxy) (resp interface{}, err error) {
+	defer proxy.Close()
+
+	executor, err := f.getExecutor(*ctx, sess, req)
+	if err != nil {
+		f.log.WithError(err).Warning("Failed creating executor.")
+		return nil, trace.Wrap(err)
+	}
+	streamOptions := proxy.options()
+
+	if err = executor.Stream(streamOptions); err != nil {
+		f.log.WithError(err).Warning("Executor failed while streaming.")
+		return nil, trace.Wrap(err)
+	}
+
 	return nil, nil
 }
 
